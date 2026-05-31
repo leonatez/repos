e7434fe1a6969f6f73275d8f977344821a4f3e39
leonatez <hailinh.leo@gmail.com>
2026-05-31 07:22:05 +0700

    Add repos.enginxlabs.com to backend CORS allowed origins

--- backend/main.py
+++ backend/main.py
@@ -63,6 +63,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
         "https://repos.crawlingrobo.com",
+        "https://repos.enginxlabs.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
